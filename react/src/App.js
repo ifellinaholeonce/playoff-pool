f169dc56e4fa2a19783262cc92bbdd059777b230
Tymm Schmitke <ifellinaholeonce@gmail.com>
2018-04-17 01:41:05 -0400

    Rearrange app

---
+++ react/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Team from './Team.jsx'
+import Game from './Game.jsx'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      players: [],
+      games: [],
+      teams: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.HOST = window.location.origin.replace(/^http/, 'ws')
+    this.connection = new WebSocket(this.HOST);
+    this.connection.onmessage = (event) => {
+      let message = JSON.parse(event.data)
+      console.log(message)
+      switch (message.type) {
+        case "init":
+          this.setState({
+            players: message.body.players,
+            games: message.body.games,
+            teams: message.body.teams
+          })
+        break;
+        case "update":
+          this.setState({
+            players: message.body.players,
+            games: message.body.games
+          })
+        break;
+      }
+    }
+  }
+  render() {
+    let teams = this.state.teams.map((team, i) => {
+      return (
+        <div key={i}>
+          <p className="App-intro">
+            {team.teamName}
+          </p>
+          <Team team={team} players={this.state.players} />
+          <p>Team Total: </p>
+        </div>
+      )
+    })
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        {teams}
+        <p className="App-intro">
+          Games
+        </p>
+        < Game games={this.state.games} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/src/Team.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+class Team extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render(){
+    let teamPlayers = this.props.team.players.map((team, i) => {
+      let player = this.props.players.find(p => {
+        return p.name === team.name
+      })
+      return(
+        <tr key={i}>
+          <th>{player.playing !== "false"? player.playing : i}</th>
+          <td>{player.name}</td>
+          <td>{player.team}</td>
+          <td>{player.score}</td>
+          <td></td>
+          <td>{player.playing !== "false"? player.goals : "-"}</td>
+          <td>{player.playing !== "false"? player.assists : "-"}</td>
+          <td>{player.playing !== "false"? Number(player.score) + Number(player.goals) + Number(player.assists) : player.score}</td>
+        </tr>
+      )
+    })
+    return(
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Team</th>
+              <th>Score Before Today</th>
+              <th></th>
+              <th>Goals Today</th>
+              <th>Assists Today</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teamPlayers}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+export default Team;
